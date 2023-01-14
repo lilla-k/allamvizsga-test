c3a82b2500d4b51e1b0f47e478f621b74ce9bac8
lilla-k <kalmarlilianna@gmail.com>
2023-01-14 14:13:15 +0100

    button

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,21 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+
 const Character = ({name}) => {
+
+    const [clicked, setClicked] = useState(false);
+
+    return(
     <div>
         <div>{name}</div>
+        <Button
+        variant="contained"
+        onClick={() => setClicked(!clicked)}
+        >{clicked?"Show less": "Show more"}</Button>
     </div>
+    )
+    
 }
 
 export default Character;
